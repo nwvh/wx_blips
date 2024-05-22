74ae35f5e6c7e954a3f0bb775ac95b3a52780e27
wx <dominik.sladecek07@gmail.com>
2024-05-22 20:35:11 +0200

    feat: multiple blips with same name, code refactor

--- client/client.lua
+++ client/client.lua
@@ -1,25 +1,27 @@
-Citizen.CreateThread(function()
-    for k,v in pairs(wx.Blips) do
-        local blip = AddBlipForCoord(v.Coords.x,v.Coords.y,v.Coords.z)
+local blips = {}
+
+CreateThread(function()
+    for name, v in pairs(wx.Blips) do
+        local blip = AddBlipForCoord(v.Coords.x, v.Coords.y, v.Coords.z)
         SetBlipSprite(blip, v.Sprite)
         SetBlipScale(blip, v.Size)
         SetBlipFlashes(blip, v.Flash)
         SetBlipColour(blip, v.Color)
         SetBlipAlpha(blip, v.Opacity)
-        SetBlipAsShortRange(blip,v.AlwaysShow)
+        SetBlipAsShortRange(blip, v.AlwaysShow)
         SetBlipHiddenOnLegend(blip, v.Hidden)
         ShowTickOnBlip(blip, v.Checkmark)
         ShowHeightOnBlip(blip, v.HeightIndicator)
         SetBlipShrink(blip, v.Shrink)
         BeginTextCommandSetBlipName('STRING')
-        AddTextComponentSubstringPlayerName(k)
+        AddTextComponentSubstringPlayerName(v.Label and v.Label or name)
         EndTextCommandSetBlipName(blip)
+        table.insert(blips, blip)
     end
 end)
 
 AddEventHandler('onResourceStop', function()
-    for k, v in pairs(wx.Blips) do
-        local blip = AddBlipForCoord(v.Coords.x,v.Coords.y,v.Coords.z)
+    for _, blip in pairs(blips) do
         RemoveBlip(blip)
     end
 end)
